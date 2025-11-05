873fb7865c3725de1d1471d1da10e956aec0bcd0
Charlie <charlieabel99@gmail.com>
2025-11-05 12:44:47 +0000

    fixed the full message logic and modified the first and second message generation

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,56 +1,77 @@
-const goodMessages = [];
-const badMessages = [];
-const statements = [];
+// arrays of statements and messages to pull from
+const statements = ['The scarecrows are watching.'];
+const goodMessages = ['You will find what you didn\'t need.'];
+const badMessages = ['You will wish you hadn\'t.'];
 
-const msgFactory = (msg='', good=true) => {
+
+// Factory function for building message objects
+const msgFactory = (msg='', gb=true) => {
     return {
         message: msg,
-        good: good
+        good: gb
     }
 
 }
 
+// returns a random 'good' message
 const goodMsg = () => {
-    i = Math.floor(Math.random()*goodMessages.length);
+    let i = Math.floor(Math.random()*goodMessages.length);
     return goodMessages[i];
 }
 
+// returns a random 'bad' message
 const badMsg = () => {
-    i = Math.floor(Math.random()*badMessages.length);
+    let i = Math.floor(Math.random()*badMessages.length);
     return badMessages[i];
 }
 
+// selects a good or bad message randomly and sends it to the factory.
 const goodOrBad = () => {
     let gb = (Math.floor(Math.random()*2));
-    gb < 1 ? msgFactory(goodMsg(),true) : msgFactory(badMsg(),false);
+    return gb < 1 ? msgFactory(goodMsg(),true) : msgFactory(badMsg(),false);
 }
 
+// selects a random statement
+const statement = () => {
+    let i = Math.floor(Math.random()*statements.length);
+    return statements[i];
+}
+
+// builds the first message and replaces the punctuation with a comma.
 const firstMsg = () => { 
     let msg = goodOrBad();
-    let conj = '';
-
     let punct = Array.from(msg.message);
     
     punct.pop();
-    punct.push(', ');
 
     msg.message = punct.join('');
+    return msg;
 }
 
+// builds the second message and de-capitalises it.
 const secondMsg = () => {
-    let msg = Array.from(goodOrBad().message)
+    let msg = goodOrBad();
+
+    msg.message = msg.message.toLowerCase();
+
+    return msg;
 }
 
-//TODO: test msg creation logic works
+
+// Works! need to make sure the two messages are not identical though. Need to check this before the messages are modified... maybe add an id or key to each message.
 const generateMessage = () => {
     let fullMsg = '';
+    let state = statement();
     let fMsg = firstMsg();
     let sMsg = secondMsg();
     let conj = '';
 
+    while (sMsg.message == fMsg.message)
+
     (fMsg.good == sMsg.good) ? conj = ' and ' : conj = ', but ';
 
-    fullMsg = statement() + fMsg.message + conj + sMsg.message;
+    fullMsg = state + ' ' + fMsg.message + conj + sMsg.message;
     console.log(fullMsg);
 }
 
+generateMessage();
